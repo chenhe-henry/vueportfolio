dbd4e4c0306cc36d09f8e5e8a65f9e74d35fd1bb
chenhe-henry <chenhe.elec@gmail.com>
2020-09-05 15:04:40 +1000

    update with theme and scss variable

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
         rounded
         :to="link.url"
       >{{ link.label }}</v-btn>
+      <v-btn @click="toggleTheme" text rounded>Toggle Theme</v-btn>
     </v-app-bar>
     <v-content>
       <router-view></router-view>
@@ -45,6 +46,12 @@ export default {
       { label: "Dashboard", url: "/dashboard" },
       { label: "Signup", url: "/signup" }
     ]
-  })
+  }),
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.themes.dark.anchor = "#41B883";
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    }
+  }
 };
 </script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -14,14 +14,77 @@
       <v-col>table-2</v-col>
     </v-row>
     <v-row>
-      <v-col cols="8">col-1</v-col>
+      <v-col cols="12" md="6">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          class="elevation-1"
+          @click:row="selectRow"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          class="elevation-1"
+          @click:row="selectRow"
+        ></v-data-table>
+      </v-col>
+      <v-col>table-2</v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          class="elevation-1"
+          @click:row="selectRow"
+        ></v-data-table>
+      </v-col>
+      <v-col>table-2</v-col>
+    </v-row>
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="8">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          class="elevation-1"
+          @click:row="selectRow"
+        ></v-data-table>
+      </v-col>
       <v-col cols="2">col-2</v-col>
       <v-col cols="1">col-3</v-col>
       <v-col cols="1">col-4</v-col>
     </v-row>
+    <v-row v-if="loadNewContent" id="more-content">
+      <v-col cols="8">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          class="elevation-1"
+          @click:row="selectRow"
+        ></v-data-table>
+      </v-col>
+      <v-col cols="4">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          class="elevation-1"
+          @click:row="selectRow"
+        ></v-data-table>
+      </v-col>
+    </v-row>
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ currentItem }}
-      <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">Close</v-btn>
+      <v-btn color="pink" text @click="snackbar = false">Close</v-btn>
     </v-snackbar>
   </v-container>
 </template>
@@ -127,13 +190,17 @@ export default {
           protein: 7,
           iron: "6%"
         }
-      ]
+      ],
+      loadNewContent: false
     };
   },
   methods: {
     selectRow(event) {
       this.snackbar = true;
       this.currentItem = event.name;
+    },
+    showMoreContent(entries) {
+      this.loadNewContent = entries[0].isIntersecting;
     }
   }
 };
